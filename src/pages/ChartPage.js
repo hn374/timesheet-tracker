992544a070c26de7d576dcfc793f3998efe10f34
Hoang Nguyen <mizore742@hotmail.com>
2020-07-17 15:53:58 -0400

    Imported excel sheet and turned into JSON object

--- src/pages/ChartPage.js
+++ src/pages/ChartPage.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import './ChartPage.css';
 // import Chart from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-import Dropzone from 'react-dropzone'
+// import Dropzone from 'react-dropzone'
+import XLSX from 'xlsx';
 
 function ChartPage() {
     // const [labels, setLabels] = useState([
@@ -16,6 +17,11 @@ function ChartPage() {
     //     borderWidth: 2,
     //     data: [65, 59, 80, 81, 56]
     //   }]);
+
+    const [labels, setLabels] = useState([]); // Set to all the titles of the skills
+    const [dates, setDates] = useState([]); // Set to all the dates
+
+    // Create an object that has skill name, with a dictionary of date:hours
 
     const [data, setData] = useState({
         labels: ['January', 'February', 'March',
@@ -31,19 +37,50 @@ function ChartPage() {
         ]
       });
 
+    function convertExcelToJson(event) {
+        var files = event.target.files; // Grab all the work sheets from the excel file
+        var fileSheet = files[0]; // Grab the first file
+
+        const reader = new FileReader(); // Create a file reading
+
+        reader.onload = (event) => { // event is when the file is selected
+            /* Parse data */
+            const binaryString = event.target.result;
+            const workBook = XLSX.read(binaryString, {
+                type:'binary'
+            });
+
+            /* Get first worksheet */
+            const workSheetName = workBook.SheetNames[0];
+            const workSheet = workBook.Sheets[workSheetName];
+
+            /* Convert array of arrays */
+            // const data = XLSX.utils.sheet_to_json(workSheet, {header:1});
+            const data = XLSX.utils.sheet_to_json(workSheet, {header:1});
+
+            console.log("Data:", data);
+
+            // Set skill labels
+            var skillLabels = data[2];
+            skillLabels.shift();
+            console.log(skillLabels);
+            setLabels(skillLabels);
+
+            // console.log("Skill Names", data[2]);
+        };
+
+        // reader.readAsDataURL(event.target.files[0]);
+        reader.readAsBinaryString(fileSheet);
+    }
+
     return(
         <div className="chartPageContainer">
             <h1>Time Sheet Charts</h1>
-            <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
-                {({getRootProps, getInputProps}) => (
-                    <section>
-                    <div className="dropZone" {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        <p>Drag 'n' drop some files here, or click to select files</p>
-                    </div>
-                    </section>
-                )}
-            </Dropzone>
+            <input
+                accept=".xlsx"
+                type="file"
+                onChange={(event) => convertExcelToJson(event)}
+            />
             <Bar
             data={data}
             options={{
